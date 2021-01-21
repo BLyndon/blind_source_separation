e5cfaf138a7f9924f2f60f936e5c694a7cbf67b0
Ben <ben@Rubens-MBP.fritz.box>
2021-01-21 18:05:35 +0100

    change to convenient input output format

--- modules/permutation.py
+++ modules/permutation.py
@@ -2,17 +2,20 @@ import numpy as np
 from numpy.linalg import *
 
 def TDOA_estimator(Wf, J=0):
-    W,f = Wf
-    try:
-        A = inv(W)
-    except:
-        A = pinv(W)
+    def single_f_est(wf,J=J): 
+        W,f = wf
+        try:
+            A = inv(W)
+        except:
+            A = pinv(W)
 
-    M = A.shape[0]
-    a_J = A[J,:]
-    A_J = np.repeat([a_J],M,axis=0)
+        M = A.shape[0]
+        a_J = A[J,:]
+        A_J = np.repeat([a_J],M,axis=0)
 
-    estimator = - np.angle(A/A_J)/(2*np.pi*f)
-    estimator = np.delete(estimator,J,0)
+        estimator = - np.angle(A/A_J)/(2*np.pi*f)
+        estimator = np.delete(estimator,J,0)
 
-    return estimator
+        return estimator
+    rf_est = list(map(single_f_est, Wf))
+    return np.squeeze(np.asarray(rf_est))
