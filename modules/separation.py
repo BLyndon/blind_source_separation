6af005b6ead68c93e71bbe52955e86a2026cb559
Ben <ben@Rubens-MBP.fritz.box>
2021-01-31 05:37:32 +0100

    add separation fct

--- modules/separation.py
+++ modules/separation.py
@@ -1,8 +1,12 @@
 import numpy as np
 from numpy.linalg import *
 import matplotlib.pyplot as plt
+from auxiliaries import timit
+from tqdm.notebook import trange, tqdm
+from time import sleep
 
 import warnings
+
 
 def sym_decorrelation(W):
     """ Symmetric decorrelation
@@ -16,18 +20,21 @@ def whitening(X):
     """ Whitening transformation
     Transform random vector X to whitened random vector Y=VX with unit diagonal covariance
     """
-    X -= X.mean(1,keepdims=True)
+    X -= X.mean(1, keepdims=True)
     w, v = eigh(np.cov(X))
     D = np.diag(w)
     V = np.sqrt(inv(D)).dot(v.conj().T)
     Z = V.dot(X)
     return V, Z
 
+
 def complex_FastICA(Z, U=None, tol=1e-6, max_iter=1000, alpha=0.1, history=True):
     def _g(y, alpha=alpha):
-        g = y.conj()/(2 * np.sqrt(np.abs(y)**2 + alpha)) 
-        dg = ((1 - 0.5 * (np.abs(y)**2/(np.abs(y)**2 + alpha)))/(2 * np.sqrt(np.abs(y)**2 + alpha))).mean(1)
+        g = y.conj()/(2 * np.sqrt(np.abs(y)**2 + alpha))
+        dg = ((1 - 0.5 * (np.abs(y)**2/(np.abs(y)**2 + alpha))) /
+              (2 * np.sqrt(np.abs(y)**2 + alpha))).mean(1)
         return g, dg
+
     def _logcosh(Y, alpha=alpha):
         Y *= alpha
         g = np.tanh(Y, Y)
@@ -37,16 +44,16 @@ def complex_FastICA(Z, U=None, tol=1e-6, max_iter=1000, alpha=0.1, history=True)
         return g, dg
 
     N, M = Z.shape
-    
+
     if history:
         lims = list()
 
-    if U==None:
-        U = np.asarray(np.random.randn(N,N), dtype=Z.dtype)
+    if U == None:
+        U = np.asarray(np.random.randn(N, N), dtype=Z.dtype)
         U = sym_decorrelation(U)
 
     for _ in range(max_iter):
-        if Z.dtype==np.complex:
+        if Z.dtype == np.complex:
             g, dg = _g(U.dot(Z))
         else:
             g, dg = _logcosh(U.dot(Z))
@@ -73,11 +80,12 @@ def complex_FastICA(Z, U=None, tol=1e-6, max_iter=1000, alpha=0.1, history=True)
 
     return U, Y
 
-def MLE(X, V, U, step_size=1e-5, max_iter=1000, tol=1e-5, history=True):
+
+def MLE(X, V, U, step_size=1e-5, max_iter=1000, tol=1e-6, history=True):
     def _g(y):
         return y/abs(y)
-        
-    N, _ = X.shape
+
+    N, M = X.shape
 
     if history:
         lims = list()
@@ -86,13 +94,13 @@ def MLE(X, V, U, step_size=1e-5, max_iter=1000, tol=1e-5, history=True):
 
     for _ in range(max_iter):
         Y = W.dot(X)
-        
-        if X.dtype==np.complex:
+
+        if X.dtype == np.complex:
             phi = _g(Y)
         else:
             phi = np.tanh(Y)
-        
-        step = step_size*(np.eye(N) - phi.dot(Y.conj().T)).dot(W)
+
+        step = step_size*(np.eye(N) - phi.dot(Y.conj().T)/M).dot(W)
         W1 = W + step
 
         lim = np.max(abs(step))
@@ -113,3 +121,21 @@ def MLE(X, V, U, step_size=1e-5, max_iter=1000, tol=1e-5, history=True):
     Y = W.dot(X)
 
     return W, Y
+
+
+@timit
+def source_separation(Xfn, f, max_iter=1000):
+    Yfn = np.zeros_like(Xfn)
+    Wf = np.zeros((Xfn.shape[1], Xfn.shape[0],
+                   Xfn.shape[0]), dtype=np.complex128)
+    for i in trange(Xfn.shape[1], desc='1st loop'):
+        Xn = Xfn[:, i, :]
+
+        V, Z = whitening(Xn)
+        U, _ = complex_FastICA(Z, max_iter=max_iter, history=False)
+        W, Y = MLE(Xn, V, U, max_iter=max_iter,
+                   step_size=1e-5, tol=1e-7, history=False)
+
+        Yfn[:, i, :] = Y
+        Wf[i, :, :] = W
+    return Wf, Yfn
