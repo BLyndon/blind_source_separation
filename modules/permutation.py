78f2a73aa0c238bf2f2d5d522982df0068e0f926
Ben <ben@Rubens-MBP.fritz.box>
2021-01-31 05:36:22 +0100

    add permutation and scaling fcts

--- modules/permutation.py
+++ modules/permutation.py
@@ -1,43 +1,80 @@
 import numpy as np
-from numpy.linalg import *
-from auxiliaries import timit, freq_list_decorator
+from numpy.linalg import norm
+from auxiliaries import timit, freq_list_decorator, get_unmixing
+from scipy.sparse import coo_matrix
+from itertools import permutations
+
 
 @freq_list_decorator
 def TDOA_estimator(Wf, J=0):
-    W,f = Wf
-    try:
-        A = inv(W)
-    except:
-        A = pinv(W)
+    W, f = Wf
+    A = get_unmixing(W)
 
     M = A.shape[0]
-    a_J = A[J,:]
-    A_J = np.repeat([a_J],M,axis=0)
+    a_J = A[J, :]
+    A_J = np.repeat([a_J], M, axis=0)
 
     estimator = - np.angle(A/A_J)/(2*np.pi*f)
-    estimator = np.delete(estimator,J,0)
+    estimator = np.delete(estimator, J, 0)
 
     return estimator
 
-from itertools import permutations
 
 @freq_list_decorator
-def find_permutations(r,c):
-    def sum_sq(v,c,perm):
-        sum=0
+def find_permutations(r, c):
+    def sum_sq(v, c, perm):
+        sum = 0
         for k in range(c.shape[0]):
             sum += norm(v[perm[k]]-c[k])**2
         return sum
-    perms = list(permutations(range(len(c))))
-    sums = [sum_sq(r,c,perm) for perm in perms]
+    perms = list(permutations(range(c.shape[0])))
+    sums = [sum_sq(r, c, perm) for perm in perms]
     return perms[np.argmin(sums)]
 
-from scipy.sparse import coo_matrix
 
 @freq_list_decorator
 def get_permutation_matrix(perm):
-    K = len(perm)
+    K = perm.shape[0]
     rows = list(range(K))
     cols = perm
     data = np.ones(K)
-    return coo_matrix((data,(rows,cols)),shape=(K, K)).toarray()
+    return coo_matrix((data, (rows, cols)), shape=(K, K)).toarray()
+
+
+@freq_list_decorator
+def permute_W(Wf, P):
+    W, f = Wf
+
+    W_ = W.dot(P.T)
+
+    Wf_ = [W_, f]
+    return Wf_
+
+
+@freq_list_decorator
+def permute_Y(Yf, P):
+    Y, f = Yf
+    Y_ = P.dot(Y)
+    Yf_ = [Y_, f]
+    return Yf_
+
+
+@freq_list_decorator
+def scale(Wf, Yf, J=0):
+    Y, f = Yf
+
+    A = get_unmixing(Wf[0])
+
+    a_J = np.reshape(A[J, :], (-1, 1))
+    Y_ = a_J*Y
+
+    return [Y_, f]
+
+
+def perm_alignment(r, Wf, Yf, perms):
+    Pf = get_permutation_matrix(perms)
+
+    Wf_ = permute_W(Wf, Pf)
+    Yf_ = permute_Y(Yf, Pf)
+
+    return Wf_, Yf_
