a32992fa7fa8937592c71d116eee0734fd7b39fa
Ben <ben@Rubens-MBP.fritz.box>
2021-01-31 05:32:45 +0100

    add masking module

---
+++ modules/tf_masking.py
@@ -0,0 +1,35 @@
+import numpy as np
+from separation import whitening
+from auxiliaries import freq_list_decorator, get_unmixing, get_Y
+
+
+@freq_list_decorator
+def get_theta(X, Wf):
+    W = Wf[0]
+
+    A = get_unmixing(W)
+    V, Z = whitening(X)
+    B = V.dot(A)
+
+    BZ = B.conj().T.dot(Z)
+    BZ_norm = np.sqrt(BZ*BZ.conj())
+
+    B_norm = np.sqrt((B*B.conj()).sum(axis=0, keepdims=True)).T
+    Z_norm = np.sqrt((Z*Z.conj()).sum(axis=0, keepdims=True))
+
+    B_Z_norm = B_norm*Z_norm
+
+    phi = BZ_norm/B_Z_norm
+    return np.arccos(phi.astype(np.float64))
+
+
+def mask(Xfn, Yf, Wf, theta_max=0.5):
+    Xf = [Xfn[:, i, :] for i in range(1, Xfn.shape[1])]
+
+    theta = get_theta(Xf, Wf)
+
+    M = np.where(theta < theta_max, 1, 0)
+    M = np.swapaxes(M, 0, 1)
+
+    YY = get_Y(Yf)
+    return M*YY
