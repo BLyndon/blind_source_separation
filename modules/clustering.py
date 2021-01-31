cb8e8cb5770c70a93bd45ab35a37fc24ff0858a3
Ben <ben@Rubens-MBP.fritz.box>
2021-01-31 05:34:51 +0100

    add cluster fct

--- modules/clustering.py
+++ modules/clustering.py
@@ -1,15 +1,33 @@
 import numpy as np
-from permutation import get_permutation_matrix
+from scipy.sparse import coo_matrix
+from auxiliaries import freq_list_decorator
+from permutation import get_permutation_matrix, find_permutations
+
 
 def init_centers(r):
     N, K = r.shape
     scale = abs(r).mean()
-    c = scale*np.random.randn(K,1)
-    return np.repeat(c,N, axis=1).T
+    c = scale*np.random.randn(K, 1)
+    return np.repeat(c, N, axis=1).T
+
 
 def update_centroids(r, perms):
-    F, K = r.shape
+    F, _ = r.shape
     P = get_permutation_matrix(perms)
-    r_perm = np.asarray(list(map(np.dot, P,r)))
-    c = np.reshape(r_perm.mean(0),(-1,1))
+    r_perm = np.asarray(list(map(np.dot, P, r)))
+    c = np.reshape(r_perm.mean(0), (-1, 1))
     return np.repeat(c, F, axis=1).T
+
+
+def cluster(r, max_iter=100):
+    c = init_centers(r)
+    for i in range(max_iter):
+        c_ = c
+        perms = find_permutations(r, c)
+        c = update_centroids(r, perms)
+
+        dc = abs(c-c_)
+        if dc.all() < 1e-12:
+            print("Converged at itr={}".format(i))
+            break
+    return perms, c
